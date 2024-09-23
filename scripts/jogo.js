73480a4a38b761b3d8882fe32711f370c17d660c
Rafael Queiroz Vilaça <rafavilaca13@gmail.com>
2024-09-23 15:49:28 -0300

    Alterações na nomenclatura de algumas variáveis e na estrutura do código

--- scripts/jogo.js
+++ scripts/jogo.js
@@ -35,16 +35,15 @@ for (let i = 1; i < secoesDaPaginaEl.length; i++) {
 }
 
 botaoComecarEl.addEventListener('click', () => {
-    if (!estaOculto(maisInformacoes.modalEl)) {
+    if (!estaOculto(maisInformacoes.modalEl))
         return;
-    }
+
     inicioDoJogoEl.classList.add('ocultar');
-    if (localStorage.getItem('partida') !== null) {
+    if (localStorage.getItem('partida') !== null)
         modalRecuperarProgressoEl.classList.remove('ocultar');
-    }
-    else {
+    else
         modalVsAmigoEl.classList.remove('ocultar');
-    }
+
     configuracoes.botaoAbrirEl.classList.remove('hidden');
 });
 
@@ -68,31 +67,32 @@ opcaoXJogadorDoisEl.addEventListener('click', () => {
     opcaoBolinhaJogadorDoisEl.classList.remove('marcar-opcao');
 });
 
+// função para verificar se alguma opção foi marcada
 function marcada(opcaoEl) {
     return (opcaoEl.classList.contains('marcar-opcao'));
 }
 
+// função para verificar se algum modal está oculto
 function estaOculto(modalEl) {
     return (modalEl.classList.contains('ocultar'));
 }
 
+// função que carrega os nomes na partida, de acordo com suas respectivas escolhas(O ou X)
 function carregarNomesNaPartida() {
-    if (marcada(opcaoXJogadorDoisEl)) {
+    if (marcada(opcaoXJogadorDoisEl))
         nomeEscolheuXEl.innerHTML = nomeJogadorDoisEl.value;
-    }
-    else {
+    else
         nomeEscolheuBolinhaEl.innerHTML = nomeJogadorDoisEl.value;
-    }
 
-    if (marcada(opcaoBolinhaJogadorUmEl)) {
+    if (marcada(opcaoBolinhaJogadorUmEl))
         nomeEscolheuBolinhaEl.innerHTML = nomeJogadorUmEl.value;
-    }
-    else {
+    else
         nomeEscolheuXEl.innerHTML = nomeJogadorUmEl.value;
-    }
+
     nomeJogadorUmEl.value = nomeJogadorDoisEl.value = '';
 }
 
+// função que limpa as opções marcadas
 function desmarcarOpcoes() {
     opcaoBolinhaJogadorDoisEl.classList.remove('marcar-opcao');
     opcaoXJogadorDoisEl.classList.remove('marcar-opcao');
@@ -100,7 +100,8 @@ function desmarcarOpcoes() {
     opcaoBolinhaJogadorUmEl.classList.remove('marcar-opcao');
 }
 
-function marcaVezDoPrimeiroAJogar() {
+// função para marcar a vez do primeiro a jogar
+function marcaVezPrimeiroJogar() {
     if (primeiroAComecar === 'O') {
         nomeEscolheuBolinhaEl.classList.add('marcar-vez');
         divBolinhaEl.classList.add('marcar-vez');
@@ -115,17 +116,18 @@ botaoJogarVsAmigoEl.addEventListener('click', () => {
     if (!marcada(opcaoBolinhaJogadorUmEl) && !marcada(opcaoXJogadorUmEl) ||
         !marcada(opcaoXJogadorDoisEl) && !marcada(opcaoBolinhaJogadorDoisEl) ||
         marcada(opcaoXJogadorUmEl) && marcada(opcaoXJogadorDoisEl) ||
-        marcada(opcaoBolinhaJogadorUmEl) && marcada(opcaoBolinhaJogadorDoisEl)) {
+        marcada(opcaoBolinhaJogadorUmEl) && marcada(opcaoBolinhaJogadorDoisEl))
         return;
-    }
+
     audioInicial.pause();
     modalVsAmigoEl.classList.add('ocultar');
     jogoEl.classList.remove('ocultar');
     carregarNomesNaPartida();
     desmarcarOpcoes();
-    marcaVezDoPrimeiroAJogar();
+    marcaVezPrimeiroJogar();
 });
 
+//função para restaurar um jogo salvo com localStorage
 function restauraPartidaSalva(jogo) {
     vitoriasBolinhaEl.innerHTML = vitoriasBolinha = jogo.vitoriasBolinha;
     vitoriasXEl.innerHTML = vitoriasX = jogo.vitoriasX;
@@ -148,58 +150,19 @@ botaoNaoRecuperarProgressoEl.addEventListener('click', () => {
     localStorage.removeItem('partida');
 });
 
+// função para criar uma matriz de celulas(apenas para facilitar)
 function criarMatrizDeCelulas() {
     let tabuleiro = [[, , ,], [, , ,], [, , ,]];
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
+    for (let i = 0; i < 3; i++)
+        for (let j = 0; j < 3; j++)
             tabuleiro[i][j] = arrCelulasEl[(i * 3) + j];
-        }
-    }
+
     return tabuleiro;
 }
 
 let tabuleiro = criarMatrizDeCelulas();
 
-function verificarLinhas() {
-    for (let i = 0; i < 3; i++) {
-        if (tabuleiro[i][0].innerHTML === vez && tabuleiro[i][1].innerHTML === vez && tabuleiro[i][2].innerHTML === vez) {
-            tabuleiro[i][0].classList.add('expandir-retrair');
-            tabuleiro[i][1].classList.add('expandir-retrair');
-            tabuleiro[i][2].classList.add('expandir-retrair');
-            return true;
-        }
-    }
-    return false;
-}
-
-function verificarColunas() {
-    for (let j = 0; j < 3; j++) {
-        if (tabuleiro[0][j].innerHTML === vez && tabuleiro[1][j].innerHTML === vez && tabuleiro[2][j].innerHTML === vez) {
-            tabuleiro[0][j].classList.add('expandir-retrair');
-            tabuleiro[1][j].classList.add('expandir-retrair');
-            tabuleiro[2][j].classList.add('expandir-retrair');
-            return true;
-        }
-    }
-    return false;
-}
-
-function verificarDiagonais() {
-    if (tabuleiro[0][0].innerHTML === vez && tabuleiro[1][1].innerHTML === vez && tabuleiro[2][2].innerHTML === vez) {
-        tabuleiro[0][0].classList.add('expandir-retrair');
-        tabuleiro[1][1].classList.add('expandir-retrair');
-        tabuleiro[2][2].classList.add('expandir-retrair');
-        return true;
-    }
-    if (tabuleiro[0][2].innerHTML === vez && tabuleiro[1][1].innerHTML === vez && tabuleiro[2][0].innerHTML === vez) {
-        tabuleiro[0][2].classList.add('expandir-retrair');
-        tabuleiro[1][1].classList.add('expandir-retrair');
-        tabuleiro[2][0].classList.add('expandir-retrair');
-        return true;
-    }
-    return false;
-}
-
+// função para alternar a vez
 function alternar(peca) {
     if (peca === 'O') {
         peca = 'X';
@@ -219,33 +182,14 @@ function alternar(peca) {
 }
 
 // função para sortear quem começa o jogo pela primeira vez (caso o usuário não tenha salvo o jogo)
-function sortearPrimeiroAComecar() {
+function sortearPrimeiroComecar() {
     let numero = Math.random();
     let primeiroComecar;
     primeiroComecar = (numero <= 0.5) ? 'O' : 'X';
     return primeiroComecar;
 }
 
-function empate() {
-    for (let celulaEl of arrCelulasEl) {
-        if (celulaEl.innerHTML === '') {
-            return false;
-        }
-    }
-    return true;
-}
-
-function contarVitoria(pecaVencedora) {
-    if (pecaVencedora === 'O') {
-        vitoriasBolinha++;
-        vitoriasBolinhaEl.innerHTML = vitoriasBolinha;
-    }
-    else {
-        vitoriasX++;
-        vitoriasXEl.innerHTML = vitoriasX;
-    }
-}
-
+// função para limpar as células do tabuleiro
 function limparCelulas() {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
@@ -257,6 +201,7 @@ function limparCelulas() {
     contJogadas = 0;
 }
 
+// função para remover a classe marcar-vez
 function desmarcarVez() {
     divBolinhaEl.classList.remove('marcar-vez');
     divXEl.classList.remove('marcar-vez');
@@ -264,19 +209,83 @@ function desmarcarVez() {
     nomeEscolheuXEl.classList.remove('marcar-vez');
 }
 
-let vez, primeiroAComecar = sortearPrimeiroAComecar();
+let vez, primeiroAComecar = sortearPrimeiroComecar();
 let contJogadas = 0;
 vez = primeiroAComecar;
 let vitoriasBolinha = 0, vitoriasX = 0;
 let temVencedor;
 
+// controla todo o jogo
 function jogo(e) {
+    // função para validar as linhas do tabuleiro
+    function verificarLinhas() {
+        for (let i = 0; i < 3; i++) {
+            if (tabuleiro[i][0].innerHTML === vez && tabuleiro[i][1].innerHTML === vez && tabuleiro[i][2].innerHTML === vez) {
+                tabuleiro[i][0].classList.add('expandir-retrair');
+                tabuleiro[i][1].classList.add('expandir-retrair');
+                tabuleiro[i][2].classList.add('expandir-retrair');
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // função para validar as colunas do tabuleiro
+    function verificarColunas() {
+        for (let j = 0; j < 3; j++) {
+            if (tabuleiro[0][j].innerHTML === vez && tabuleiro[1][j].innerHTML === vez && tabuleiro[2][j].innerHTML === vez) {
+                tabuleiro[0][j].classList.add('expandir-retrair');
+                tabuleiro[1][j].classList.add('expandir-retrair');
+                tabuleiro[2][j].classList.add('expandir-retrair');
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // função para validar as diagonais do tabuleiro
+    function verificarDiagonais() {
+        if (tabuleiro[0][0].innerHTML === vez && tabuleiro[1][1].innerHTML === vez && tabuleiro[2][2].innerHTML === vez) {
+            tabuleiro[0][0].classList.add('expandir-retrair');
+            tabuleiro[1][1].classList.add('expandir-retrair');
+            tabuleiro[2][2].classList.add('expandir-retrair');
+            return true;
+        }
+        if (tabuleiro[0][2].innerHTML === vez && tabuleiro[1][1].innerHTML === vez && tabuleiro[2][0].innerHTML === vez) {
+            tabuleiro[0][2].classList.add('expandir-retrair');
+            tabuleiro[1][1].classList.add('expandir-retrair');
+            tabuleiro[2][0].classList.add('expandir-retrair');
+            return true;
+        }
+        return false;
+    }
+
+    // função que incrementa o número de vitorias da peça vencedora
+    function contarVitoria(pecaVencedora) {
+        if (pecaVencedora === 'O') {
+            vitoriasBolinha++;
+            vitoriasBolinhaEl.innerHTML = vitoriasBolinha;
+        }
+        else {
+            vitoriasX++;
+            vitoriasXEl.innerHTML = vitoriasX;
+        }
+    }
+
+    // função para verificar o empate
+    function empate() {
+        for (let celulaEl of arrCelulasEl)
+            if (celulaEl.innerHTML === '')
+                return false;
+
+        return true;
+    }
+
     let celulaRecuperadaEl = e.currentTarget;
 
     if (celulaRecuperadaEl.innerHTML !== '' || temVencedor || !estaOculto(configuracoes.modalEl)
-        || mainPrincipalEl.classList.contains('capa-escura-modal')) {
+        || mainPrincipalEl.classList.contains('capa-escura-modal'))
         return;
-    }
 
     reproduzir(clickNaCelula);
     contJogadas++;
@@ -299,9 +308,9 @@ function jogo(e) {
         configuracoes.botaoAbrirEl.classList.add('hidden');
         maisInformacoes.linkEl.classList.add('hidden');
         setTimeout(() => {
-            for (let celulaEl of arrCelulasEl) {
+            for (let celulaEl of arrCelulasEl)
                 celulaEl.style.cursor = 'auto';
-            }
+
             abrirModal(modalContinuarOuSairDoJogoEl);
             desmarcarVez();
         }, tempoDeAtraso);
@@ -332,6 +341,7 @@ botaoSairDoJogoEl.addEventListener('click', () => {
     temVencedor = false;
 });
 
+// função para salvar a partida no localStorage
 function salvarPartida() {
     let partida = {
         nomeEscolheuBolinha: nomeEscolheuBolinhaEl.innerHTML,
@@ -344,6 +354,7 @@ function salvarPartida() {
     localStorage.setItem('partida', JSON.stringify(partida));
 }
 
+// função para zerar a partida
 function zerarPartida() {
     modalSalvarOuDescartarProgressoEl.classList.add('ocultar');
     mainPrincipalEl.classList.remove('capa-escura-modal');
@@ -357,7 +368,8 @@ function zerarPartida() {
     }
 }
 
-function redirecionaParaTelaInicial() {
+// função para redirecionar para a tela inicial
+function redirecionaTelaInicial() {
     inicioDoJogoEl.classList.remove('ocultar');
     maisInformacoes.linkEl.classList.remove('hidden');
     configuracoes.botaoAbrirEl.classList.add('hidden');
@@ -367,25 +379,26 @@ function redirecionaParaTelaInicial() {
 botaoSalvarProgressoEl.addEventListener('click', () => {
     salvarPartida();
     zerarPartida();
-    redirecionaParaTelaInicial();
+    redirecionaTelaInicial();
 });
 
 botaoExcluirProgressoEl.addEventListener('click', () => {
     zerarPartida();
-    redirecionaParaTelaInicial();
+    redirecionaTelaInicial();
 });
 
-function redefineValorDoCursorDaPartida(valorDoCursor) {
-    for (let celulaEl of arrCelulasEl) {
-        celulaEl.style.cursor = valorDoCursor;
-    }
-    botaoJogarVsAmigoEl.style.cursor = valorDoCursor;
+// função que redefine o valor do cursor da partida
+function redefineValorCursorPartida(valorCursor) {
+    for (let celulaEl of arrCelulasEl)
+        celulaEl.style.cursor = valorCursor;
+
+    botaoJogarVsAmigoEl.style.cursor = valorCursor;
 }
 
 configuracoes.botaoAbrirEl.addEventListener('click', () => {
-    redefineValorDoCursorDaPartida('auto');
+    redefineValorCursorPartida('auto');
 });
 
 configuracoes.botaoFecharEl.addEventListener('click', () => {
-    redefineValorDoCursorDaPartida('pointer');
+    redefineValorCursorPartida('pointer');
 });
